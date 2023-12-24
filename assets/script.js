1d715f3fd76edb178f4481eaf497dd8f10df3c8d
Gabriel Martins <39447491+gabriellsux@users.noreply.github.com>
2023-12-24 15:59:49 -0300

    Finalizando o projeto!

--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,14 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
 
 function exibirEtapa() {
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
 
     for(let i=0; i<etapa.numeros;i++) {
         if(i === 0) {
@@ -46,7 +49,11 @@ function atualizaInterface() {
 
         let fotosHtml = '';
         for(let i in candidato.fotos) {
-            fotosHtml += `<div class="d-1-image"><img src="/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            if(candidato.fotos[i].small) {
+                fotosHtml += `<div class="d-1-image small"><img src="/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            } else {
+                fotosHtml += `<div class="d-1-image"><img src="/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            }
         }
 
         lateral.innerHTML = fotosHtml;
@@ -73,15 +80,39 @@ function clicou(n) {
 }
 
 function branco() {
-    alert('Clicou em BRANCO!')
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+    lateral.innerHTML = '';
 }
 
 function corrigir() {
-    alert('Clicou em CORRIGE!')
+    exibirEtapa();
 }
 
 function confirmar() {
-    alert('Clicou em CONFIRMA!')
+    let etapa = etapas[etapaAtual];
+
+    let votoConfirmado = false;
+    if(votoBranco === true) {
+        votoConfirmado = true;
+        console.log("Confirmando como BRANCO...");
+    } else if(numero.length === etapa.numeros) {
+        votoConfirmado = true;
+        console.log("Confirmando como "+numero);
+    }
+
+    if(votoConfirmado) {
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined) {
+            exibirEtapa();
+        } else {
+            console.log("FIM!");
+        } 
+    }
 }
 
 exibirEtapa();
